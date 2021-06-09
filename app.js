38f78434cf1ef6f0389947dcd4170d1e9dc0c480
Lochan Potdar <66880876+lochanp@users.noreply.github.com>
2021-06-09 20:50:44 +0530

    changes

--- app.js
+++ app.js
@@ -87,7 +87,8 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
+const port = process.env.PORT || 3000;
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
